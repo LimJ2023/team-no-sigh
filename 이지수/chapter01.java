aa4d584f9867c7ba4c066461210de3ea1a208f6d
usernameljs <150655135+usernameljs@users.noreply.github.com>
2023-11-13 17:14:54 +0900

    연습문제
    chapter01

---
+++ 이지수/chapter01.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class chapter01 {
+
+	public static void main(String[] args) {
+		//Q1. 컴파일링
+		//Q2. 객체지향언어
+		//Q3. JDK
+		//Q4. J
+		
+		
+		//Q5.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("이름을 입력하세요");
+		String name = scan.nextLine();
+		System.out.println("ㅎㅇ" + name);
+		
+		scan.close();
+	}
+
+}
