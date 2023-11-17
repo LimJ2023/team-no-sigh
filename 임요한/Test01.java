ca13c4ff3e2cead18acb4b7c1d2c947fa647c3f6
LimJ <149343911+LimJ2023@users.noreply.github.com>
2023-11-17 12:47:37 +0900

    Update Test01.java

--- 임요한/Test01.java
+++ 임요한/Test01.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 
 public class Test01 {
 	public static void main(String[] args) {
+
+		for (int i = 0; i < 3; i++) { //계단 찍기
+			for (int j = 0; j < 1 + i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < 5; i++) { // 역 피라미드
+
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 9 - i * 2; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 		Scanner scan = new Scanner(System.in);
 		System.out.print("홀수 입력."  );
 		int inputNum = scan.nextInt();
