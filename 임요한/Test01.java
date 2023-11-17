bec4c25dbf09bdf5bfe7dcdd7d87e68d091dd3e5
LimJ <149343911+LimJ2023@users.noreply.github.com>
2023-11-17 11:33:00 +0900

    별찍기 몇개 한거 올림(마름모추가)
    파이팅

---
+++ 임요한/Test01.java
@@ -0,0 +1,30 @@
+package chapter06_practice;
+
+import java.util.Scanner;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("홀수 입력."  );
+		int inputNum = scan.nextInt();
+		
+		for (int i = 1; i <= inputNum; i++) { // 마름모 위쪽
+			for (int j = 0; j < inputNum-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (2*i) - 1; j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+		
+		for (int i = inputNum - 1; i > 0; i--) { // 마름모 아래쪽
+			for (int j = 0; j < inputNum-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (2*i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
