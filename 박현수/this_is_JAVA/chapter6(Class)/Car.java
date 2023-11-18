5062fd6614a896114db669a7cde40b0f650b3f6c
Pack Hyun Soo <31475889+yoouoblack@users.noreply.github.com>
2023-11-18 21:54:20 +0900

    Create Car.java

---
+++ 박현수/this_is_JAVA/chapter6(Class)/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+	//필드
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
+}
+//CarExample의 클래스 파일
+//자동차의 회사, 모델명, 색상, 최고 속력, 속력에 대한 정보가 담겨 있음.
+//CarExample에서 speed 필드(변수)의 값을 60으로 변경을 선언함.  
