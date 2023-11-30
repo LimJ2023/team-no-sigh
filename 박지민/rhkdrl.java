fc72048d8f7ad8dc82494bae72ea57a8ecde9fd7
lunatic1317 <151497792+lunatic1317@users.noreply.github.com>
2023-11-30 18:08:18 +0900

    11.30 2차원 배열 과제

---
+++ 박지민/rhkdrl.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class rhkdrl {
+
+	public static void main(String[] args) {
+		
+		boolean run = true;
+		Scanner sc = new Scanner(System.in);
+		
+		while (run){
+			System.err.println("집에 가고싶다 집 보내줘 집에 가고싶다 ");
+			System.out.println("집에 가고싶다 집 보내줘 집에 가고싶다 ");
+			System.err.println("집에 가고싶다 집 보내줘 집에 가고싶다 ");
+			
+			
+		}
+		
+	}
+
+}
