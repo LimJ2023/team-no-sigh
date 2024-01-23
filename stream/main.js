f9a039980dafb4c431c3a81b0f537471adbcbfff
injikkim12 <150655299+injikkim12@users.noreply.github.com>
2024-01-23 14:42:10 +0900

    stream_main
    stream_main

---
+++ stream/main.js
@@ -0,0 +1,55 @@
+//swiper
+var swiper = new Swiper(".popular-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        280:{
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        320:{
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        510:{
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        758:{
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        900:{
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+  });
+  //show video
+  let playButton = document.querySelector('.play-movie');
+  let video = document.querySelector('.video-container');
+  let myvideo = document.querySelector('#myvideo');
+  let closebtn = document.querySelector('.close-video');
+
+  playButton.onclick = () =>{
+    video.classList.add("show-video");
+    //auto play when click on btn
+    myvideo.play();
+  };
+  closebtn.onclick = () =>{
+    video.classList.remove("show-video");
+    //pause on close button
+    myvideo.pause();
+  };
