82a57470d11968845f5958f478b37418d070e634
Lim J <heuristic2022@gmail.com>
2024-02-15 18:14:37 +0900

    트렌드 페이지에 좋아요,조회수 api 찾아옴

--- StreamerSensor0211/WebContent/resources/home/trend.js
+++ StreamerSensor0211/WebContent/resources/home/trend.js
@@ -9,11 +9,20 @@ List.items.forEach(item => {
         <img src="${item.snippet.thumbnails.default.url}" />
     </div>
     <div class="rank-info-foot">
-        <div class="rank-name" >${item.snippet.title}</div>
-        <div class="rank-views">0</div>
+        <div class="rank-name" ></div>
+        <div class="rank-views">조회수 ${item.statistics.viewCount}</div>
+        <div class="rank-likes">좋아요 ${item.statistics.likeCount}</div>
         <div class="rank-trend"></div>
     </div>
     `
     info.innerHTML = cont;
     document.querySelector("#first_list").appendChild(info);
 })
+
+
+function deleteALl() {
+    document.querySelectorAll(".rank-info").forEach(item =>{
+        item.remove
+    })
+
+  }
