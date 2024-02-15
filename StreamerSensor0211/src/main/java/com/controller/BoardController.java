67f218d60a9468d07d8e80fef6e62eb7ff613bbe
Ji Soo Lee <ljljlj4567@gmail.com>
2024-02-16 02:18:19 +0900

    0215

---
+++ StreamerSensor0211/src/main/java/com/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping(value = "/board")
+	public String boardListPage() {
+		return "/board/boardList";
+	}
+	
+	@RequestMapping(value = "/board/boardView")
+	public String boardWritePage() {
+		return "/board/boardView";
+	}
+	
+	
+}
