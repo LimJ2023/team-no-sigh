8c0701d52ac0a1a5e350d498c31e5754c3785350
cutyandkind <153469689+cutyandkind@users.noreply.github.com>
2024-02-22 18:18:49 +0900

    테스트 커밋

--- StreamerSensor0219/src/main/java/com/controller/MemberController.java
+++ StreamerSensor0219/src/main/java/com/controller/MemberController.java
@@ -12,32 +12,32 @@ import com.service.UsersService;
 
 @Controller
 public class MemberController {
-	
+
 	@Autowired
 	UsersService uService;
-	
+
 	@GetMapping("/members")
 	public String members(Model model) {
-		
+
 		List<Users> users = uService.printUsers();
 		model.addAttribute("users", users);
-		
+
 		return "admin/members";
 	}
-	
+
 	@GetMapping("/dashBoard")
 	public String dashBoard() {
 		return "admin/dashBoard";
 	}
-	
+
 	@GetMapping("/index")
 	public String index() {
 		return "home/index";
 	}
-	
+
 	@GetMapping("/member_profile")
 	public String member_profile() {
-		
+
 		return "admin/member_profile";
 	}
 
