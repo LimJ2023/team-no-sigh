c4827269490af7f1af8e0daf4c7d12bb61a503c4
usernameljs <ljljlj4567@gmail.com>
2024-02-28 19:10:59 +0900

    돌림
    HomeController / Streamer / StreamMapepr / HomeController 돌림

--- StreamerSensor0219/src/main/java/com/controller/HomeController.java
+++ StreamerSensor0219/src/main/java/com/controller/HomeController.java
@@ -1,56 +1,25 @@
 package com.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.domain.Ranking;
-import com.domain.Streamer;
-import com.service.MainService;
-import com.service.RankingService;
 
 @Controller
 public class HomeController {
 
-	//0228 이지수: 인기 방송 API 로 불러오기용
-	@Autowired
-	RankingService rankingService;
-	
-	@Autowired
-	MainService mainService;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-					//0228 이지수 추가(Model)
-		
-		
-		//0228 이지수: 홈화면 최고인기 방송 불러오기
-		List<Ranking> popVideoInfo = rankingService.popVideoApi();
-		model.addAttribute("popVideoInfo", popVideoInfo);
-		//0228 이지수: 홈화면 스트리머1(머독) 정보 불러오기
-		List<Streamer> streamerInfo = mainService.getStreamerInfo();
-		model.addAttribute("streamerInfo", streamerInfo);
-		//
-		
+	public String home() {
 		return "/home/index";
 	}
 
 	// home 링크를 누르면 index
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String home2(Model model) {
-				//0228 이지수 추가 
-		List<Ranking> popVideoInfo = rankingService.popVideoApi();
-		model.addAttribute("popVideoInfo", popVideoInfo);
-		List<Streamer> streamerInfo = mainService.getStreamerInfo();
-		model.addAttribute("streamerInfo", streamerInfo);
-		
-		
+	public String home2() {
 		return "/home/index";
 	}
 
+	
 	
 }
--- StreamerSensor0219/src/main/java/com/domain/Streamer.java
+++ StreamerSensor0219/src/main/java/com/domain/Streamer.java
@@ -2,93 +2,56 @@ package com.domain;
 
 public class Streamer {
 	
-	private int strm_num;
-	private String strm_id;
-	private String strm_gender;
-	private String strm_grade;
-	private String strm_status;
-	private String strm_platform;
-	private int strm_followers;
-	
-	//0228 이지수
-	private long video_count;
-	private String channel_title;
-	private long subscriber_count;
-	private String title;
-	//
+	private int streamer_idx;
+	private String streamer_id;
+	private String streamer_gender;
+	private String streamer_grade;
+	private String streamer_status;
+	private String streamer_platform;
+	private int streamer_followers;
 	
 	
-	public int getStrm_num() {
-		return strm_num;
+	public int getStreamer_idx() {
+		return streamer_idx;
 	}
-	public void setStrm_num(int strm_num) {
-		this.strm_num = strm_num;
+	public void setStreamer_idx(int streamer_idx) {
+		this.streamer_idx = streamer_idx;
 	}
-	public String getStrm_id() {
-		return strm_id;
+	public String getStreamer_id() {
+		return streamer_id;
 	}
-	public void setStrm_id(String strm_id) {
-		this.strm_id = strm_id;
+	public void setStreamer_id(String streamer_id) {
+		this.streamer_id = streamer_id;
 	}
-	public String getStrm_gender() {
-		return strm_gender;
+	public String getStreamer_gender() {
+		return streamer_gender;
 	}
-	public void setStrm_gender(String strm_gender) {
-		this.strm_gender = strm_gender;
+	public void setStreamer_gender(String streamer_gender) {
+		this.streamer_gender = streamer_gender;
 	}
-	public String getStrm_grade() {
-		return strm_grade;
+	public String getStreamer_grade() {
+		return streamer_grade;
 	}
-	public void setStrm_grade(String strm_grade) {
-		this.strm_grade = strm_grade;
+	public void setStreamer_grade(String streamer_grade) {
+		this.streamer_grade = streamer_grade;
 	}
-	public String getStrm_status() {
-		return strm_status;
+	public String getStreamer_status() {
+		return streamer_status;
 	}
-	public void setStrm_status(String strm_status) {
-		this.strm_status = strm_status;
+	public void setStreamer_status(String streamer_status) {
+		this.streamer_status = streamer_status;
 	}
-	public String getStrm_platform() {
-		return strm_platform;
+	public String getStreamer_platform() {
+		return streamer_platform;
 	}
-	public void setStrm_platform(String strm_platform) {
-		this.strm_platform = strm_platform;
+	public void setStreamer_platform(String streamer_platform) {
+		this.streamer_platform = streamer_platform;
 	}
-	public int getStrm_followers() {
-		return strm_followers;
+	public int getStreamer_followers() {
+		return streamer_followers;
 	}
-	public void setStrm_followers(int strm_followers) {
-		this.strm_followers = strm_followers;
-	}
-	
-	
-	//0228 이지수
-	public long getVideo_count() {
-		return video_count;
-	}
-	public void setVideo_count(long video_count) {
-		this.video_count = video_count;
-	}
-	public String getChannel_title() {
-		return channel_title;
-	}
-	public void setChannel_title(String channel_title) {
-		this.channel_title = channel_title;
-	}
-	//
-	public long getSubscriber_count() {
-		return subscriber_count;
-	}
-	public void setSubscriber_count(long subscriber_count) {
-		this.subscriber_count = subscriber_count;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
+	public void setStreamer_followers(int streamer_followers) {
+		this.streamer_followers = streamer_followers;
 	}
 	
 }
-
-
