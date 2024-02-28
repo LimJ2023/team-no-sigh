766cd8206decaedb954884d344797079cf7d11f8
usernameljs <ljljlj4567@gmail.com>
2024-02-28 18:40:12 +0900

    API 테스트 중
    API 테스트 중
    (메인 스트리머 이름 밑 주석 해제하면 로드 들어감

--- StreamerSensor0219/src/main/java/com/mapper/RankingMapper.java
+++ StreamerSensor0219/src/main/java/com/mapper/RankingMapper.java
@@ -39,17 +39,6 @@ public interface RankingMapper {
 			+ "WHERE r.rank_date = '2024-01-09'")
 	int getDaily_viewers();
 	
-	
-/*
-	//20240227 이지수 - api - DB 입력 테스트
-	@Insert("INSERT INTO streaming_img "
-			+ "VALUES (img_id_seq_test.nextval, "
-			+ "#{videoThumbUrl}")
-	*/
-	
-	//20240227 이지수 api-db 입력 테스트
-	/*@Insert("INSERT INTO streaming_info(streaming_id, streaming_description)"
-			+ "VALUES (streaming_id_test_seq.nextval, #{streaming_description}")
-	void addVideoInfo(List<Ranking> videoRankingBean);*/
+
 	
 }
