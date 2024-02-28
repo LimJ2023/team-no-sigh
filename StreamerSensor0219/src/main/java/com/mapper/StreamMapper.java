1ce8f85209eb506e35079c5557c8c7aac30f1b54
Lim J <heuristic2022@gmail.com>
2024-02-28 09:05:51 +0900

    쿼리문과 vo 수정

--- StreamerSensor0219/src/main/java/com/mapper/StreamMapper.java
+++ StreamerSensor0219/src/main/java/com/mapper/StreamMapper.java
@@ -6,7 +6,7 @@ import com.domain.StreamInfo;
 
 public interface StreamMapper {
 
-	@Select("select s.streaming_id, s.strm_id, s.streaming_desc, "
+	@Select("select s.streaming_id, s.streamer_id, s.streaming_description, "
 			+ "s.streaming_time, s.streaming_category, s.streaming_date, i.img_url "
 			+ "from streaming_info s "
 			+ "inner join streaming_img i on s.img_id = i.img_id")
